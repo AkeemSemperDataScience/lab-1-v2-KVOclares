455c7ef8951b3ceed3404080a49351bc3750778b
Kier <Kiervoclares@gmail.com>
2024-09-10 14:46:37 -0600

    Testing Q3

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -29,6 +29,10 @@ def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
     # Return the character of the string in the index given by number.  If this index does not exist, then return -1.
     character_at = -1
+    if input_number <= len(input_string)-1:
+        character_at = input_string[input_number]
+    else:
+        character_at = -1
 
     return character_at
 
